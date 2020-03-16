d6a309f2731807255538ee5f781e4cf1102b8421
Steve Rivers <steve@futrli.com>
2020-03-16 22:55:09 +0000

    Formatting

--- iterm/install.sh
+++ iterm/install.sh
@@ -13,15 +13,15 @@ dirpath="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 source "${dirpath}/../utils.sh"
 
 download() {
-  local target=$1 path=$2 filename=$3
-  if ! [[ -d ${path} ]] ; then
-    fail "Bad path for file download"
-    exit 1
-  fi
-  if ! [[ -f "${path}/${filename}" ]] ; then
-    pinfo "Downloading ${filename}"
-    curl "${target}" >> "${path}/${filename}" 2>/dev/null
-  fi
+    local target=$1 path=$2 filename=$3
+    if ! [[ -d ${path} ]] ; then
+        fail "Bad path for file download"
+        exit 1
+    fi
+    if ! [[ -f "${path}/${filename}" ]] ; then
+        pinfo "Downloading ${filename}"
+        curl "${target}" >> "${path}/${filename}" 2>/dev/null
+    fi
 }
 
 mkdir -p "${iterm_data_path}"
@@ -29,12 +29,12 @@ download "${nord_dircolors}" "${iterm_data_path}" "nord.dircolors"
 download "${nord_theme}" "${iterm_data_path}" "Nord.itermcolors"
 
 if ! [[ -f $HOME/.dircolors ]] ; then
-  pinfo "Linking Nord dircolors"
-  ln -sfv "${iterm_data_path}/nord.dircolors" "$HOME/.dircolors"
+    pinfo "Linking Nord dircolors"
+    ln -sfv "${iterm_data_path}/nord.dircolors" "$HOME/.dircolors"
 else
-  pwarn "Refusing to overwrite exisiting dircolors file"
+    pwarn "Refusing to overwrite exisiting dircolors file"
 fi
 
 if [[ -f "${iterm_data_path}/Nord.itermcolors" ]] ; then
-  pinfo "Run the following to import the theme into iterm: \"open ${iterm_data_path}/Nord.itermcolors\""
+    pinfo "Run the following to import the theme into iterm: \"open ${iterm_data_path}/Nord.itermcolors\""
 fi

a89b2dee11a254d3f0af6bb40b2594a92640e03e
Steve Rivers <steve@futrli.com>
2020-03-18 13:41:16 +0000

    Add neovim

---
+++ neovim/install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# Path to this script
+dirpath="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+source "${dirpath}/utils.sh"
+
+pinfo "Installing spacevim"
+curl -sLf https://spacevim.org/install.sh | bash
+
+pinfo "Linking SpaceVim init.toml"
+if [ -d ~/.SpaceVim.d ] ; then
+    ln -sfv "$PWD/neovim/init.toml" "$HOME/.SpaceVim.d/init.toml"
+fi
+
+

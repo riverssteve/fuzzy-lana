8faf323677984d70d4ec2b330ccfc18210be3e87
Steve Rivers <steve@futrli.com>
2021-02-02 21:00:04 +0000

    [gpg] install different gpg agent confs

--- gpg/install.sh
+++ gpg/install.sh
@@ -1,11 +1,24 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# Path to this script
+dirpath="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+source "${dirpath}/../utils.sh"
+
+# shellcheck disable=SC2086
+cd "$(dirname ${0})"
 
 test -d ~/.gnupg || {
+    AGENT=gpg-agent.conf
+    if is_mac && [[ $(uname -m) == "arm64" ]] ; then
+        AGENT=gpg-agent-arm64.conf
+    fi
     mkdir ~/.gnupg
 }
 
 test -L ~/.gnupg/gpg-agent.conf || {
-  ln -sfv "$DOTFILES/gpg/gpg-agent.conf" ~/.gnupg/gpg-agent.conf
+  ln -sfv "$DOTFILES/gpg/$AGENT" ~/.gnupg/gpg-agent.conf
 }
 
 test -L ~/.gnupg/gpg.conf || {

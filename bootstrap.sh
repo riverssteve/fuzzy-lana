49030e4695d57499c21e28a8d931db8e2bb0cbc1
Steve Rivers <steve@futrli.com>
2021-03-25 10:47:02 +0000

    [bootstrap] use functions to work out os

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,15 +2,26 @@
 
 set -euo pipefail
 
-if ! command -v curl ; then
-    if [[ $( uname ) == "Linux" ]] && [[ -n $( command -v apt-get ) ]] ; then
-        sudo apt-get update && sudo apt-get install curl
+
+function is_mac {
+    if [[ $( uname ) == "Darwin" ]] ; then 
+        echo 1
+    else
+        echo 0
     fi
-fi
+}
+
+function is_ubuntu {
+    if [[ $( uname ) == "Linux" ]] && [[ -n $( command -v apt-get ) ]] ; then
+        echo 1
+    else
+        echo 0
+    fi
+}
 
 if ! command -v git ; then
-    if [[ $( uname ) == "Linux" ]] && [[ -n $( command -v apt-get ) ]] ; then
-        sudo apt-get update && sudo apt-get install git
+    if [[ $( is_ubuntu ) -eq 1 ]] ; then
+        sudo apt-get update && sudo apt-get install -y git
     fi
 fi
 
@@ -21,8 +32,10 @@ fi
 
 # install homebrew
 if ! command -v brew ; then
-    echo "[Bootstrap] Installing Homebrew"
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    if [[ $( is_mac ) -eq 1 ]] ; then
+        echo "[Bootstrap] Installing Homebrew"
+        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    fi
 fi
 
 if [[ ! -d $HOME/.config ]]; then
@@ -42,15 +55,14 @@ if [[ ! -d $HOME/.nvm ]] ; then
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh | bash
 fi
 
-echo "[Bootstrap] Install a newer bash"
-brew install bash
-
+if [[ $( is_mac ) -eq 1 ]] ; then
+    echo "[Bootstrap] Install a newer bash"
+    brew install bash
+fi
 
 echo ""
 printf "%s\n" "Remember to set up GPG keys for git usage"
 printf "%s\n" "See https://stackoverflow.com/questions/41052538/git-error-gpg-failed-to-sign-data"
-printf "%s\n" "Also remember that if installing Karibiner Elements that Jamf needs to be updated"
-printf "%s\n" "to allow kernel extensions if this is a new device"
 
 cd "$HOME/.dotfiles"
 ./install.sh

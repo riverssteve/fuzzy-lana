86f8fde5daf5101844989e827981c1b89ade05f6
Steve Rivers <steve@futrli.com>
2021-03-26 10:57:28 +0000

    [general] checks around os-specific scripts

--- macos/install.sh
+++ macos/install.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
-if ! [[ $(uname -a) != "darwin" ]] ; then
-  exit
+set -euo pipefail
+
+# Path to this script
+dirpath="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+source "${dirpath}/../utils.sh"
+
+# shellcheck disable=SC2086
+cd "$(dirname ${0})"
+
+if ! is_mac ; then
+    exit
 fi
 
 defaults write com.apple.screencapture location ~/Downloads/
